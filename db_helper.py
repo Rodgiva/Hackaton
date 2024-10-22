f56fd0d947030b9c7aed84b493cc586dc8c8bd3c
Avigdor <avigdorpartouche@gmail.com>
2024-10-22 13:09:21 +0300

    .

--- db_helper.py
+++ db_helper.py
@@ -1,18 +1,14 @@
 import psycopg2
 
-DB_NAME = "Hackaton"
-USER = "postgres"
-PASSWORD = "917364"
-HOST = "localhost"
-PORT = "5432"
+DB_NAME = ""
+USER = ""
+PASSWORD = ""
+HOST = ""
+PORT = ""
 
 try:
     connection = psycopg2.connect(
-        dbname = DB_NAME,
-        user = USER,
-        password = PASSWORD,
-        host = HOST,
-        port = PORT
+        dbname=DB_NAME, user=USER, password=PASSWORD, host=HOST, port=PORT
     )
 
 except Exception as err:
@@ -20,30 +16,32 @@ except Exception as err:
 
 player_id = None
 
+
 class CRUD_Player:
     player_id = None
-    def __init__(self, name:str, strength:int, agility:int, speed:int) -> None:
+
+    def __init__(self, name: str, strength: int, agility: int, speed: int) -> None:
         self.name = name
         self.strength = strength
         self.agility = agility
         self.speed = speed
-    
+
     def save(self):
-        query = f'''
+        query = f"""
         insert into Player (name, strength, agility, speed)
         values (%s, %s, %s, %s)
-        '''
+        """
         cursor = connection.cursor()
         cursor.execute(query, (self.name, self.strength, self.agility, self.speed))
         CRUD_Player.player_id = cursor.lastrowid
         connection.commit()
         cursor.close()
-    
+
     def delete(self, name):
-        query = f'''
+        query = f"""
         delete from Player
         where item_name = '{name}'
-        '''
+        """
         try:
             cursor = connection.cursor()
             cursor.execute(query)
@@ -54,13 +52,13 @@ class CRUD_Player:
             return False
         else:
             return True
-    
+
     @staticmethod
-    def get_player(id:int):
-        query = f'''
+    def get_player(id: int):
+        query = f"""
         select * from Player
         where player_id = {id}
-        '''
+        """
         cursor = connection.cursor()
         cursor.execute(query)
         output = cursor.fetchall()
@@ -70,30 +68,36 @@ class CRUD_Player:
             return None
 
         return output
-        
+
+
 class CRUD_Monster:
-    def __init__(self, name:str, strength:int, agility:int, speed:int, player_id:int) -> None:
+    def __init__(
+        self, name: str, strength: int, agility: int, speed: int, player_id: int
+    ) -> None:
         self.name = name
         self.strength = strength
         self.agility = agility
         self.speed = speed
         self.player_id = player_id
-    
+
     def save(self):
-        query = f'''
+        query = f"""
         insert into Monster (name, strength, agility, speed, player_id)
         values (%s, %s, %s, %s, %s)
-        '''
+        """
         cursor = connection.cursor()
-        cursor.execute(query, (self.name, self.strength, self.agility, self.speed, CRUD_Player.player_id))
+        cursor.execute(
+            query,
+            (self.name, self.strength, self.agility, self.speed, CRUD_Player.player_id),
+        )
         connection.commit()
         cursor.close()
-    
+
     def delete(self, name):
-        query = f'''
+        query = f"""
         delete from Monster
         where item_name = '{name}'
-        '''
+        """
         try:
             cursor = connection.cursor()
             cursor.execute(query)
@@ -104,13 +108,13 @@ class CRUD_Monster:
             return False
         else:
             return True
-    
+
     @staticmethod
-    def get_monsters_by_player(id:int):
-        query = f'''
+    def get_monsters_by_player(id: int):
+        query = f"""
         select * from Player
         where player_id = {id}
-        '''
+        """
         cursor = connection.cursor()
         cursor.execute(query)
         output = cursor.fetchall()
